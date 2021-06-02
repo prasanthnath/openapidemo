f3e02fb58789a99bcf1e1ad9d4905ce7a2f9bb4a
Prasanth Gullapalli <gullapal@arcesium.com>
2021-06-02 16:15:26 +0530

    README.md added

--- apiserver/src/main/java/com/xyz/openapi/server/controller/OrganizationController.java
+++ apiserver/src/main/java/com/xyz/openapi/server/controller/OrganizationController.java
@@ -4,7 +4,6 @@ import com.xyz.openapi.server.model.Department;
 import com.xyz.openapi.server.model.Employee;
 import com.xyz.openapi.server.model.EmployeeList;
 import com.xyz.openapi.server.model.ErrorResponse;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
